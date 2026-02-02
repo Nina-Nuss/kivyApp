53a315586e288e0c907a4d92783f3877bd809669
JonAcount <jonst100@gmail.com>
2026-02-02 10:32:25 +0100

    Test

---
+++ testKVDatei.py
@@ -0,0 +1,17 @@
+# from kivy.app import App
+# from kivy.uix.gridlayout import GridLayout
+# #from kivy.properties import ObjectProperty, StringProperty, ListProperty
+
+
+import kivy
+# kivy.require('1.0.7')
+
+from kivy.app import App
+
+
+class startApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    startApp().run()

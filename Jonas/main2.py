df75fbd47bc6f34f26afcec7e65fbf93adb13b7d
Your Name <you@example.com>
2026-02-20 14:01:21 +0100

    .

--- Jonas/main2.py
+++ Jonas/main2.py
@@ -1,14 +1,23 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
 import kivy
 from kivy.app import App
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.properties import ObjectProperty, StringProperty, ListProperty
+from nina.db.selectMap import get_level_map
+
 
 version = "Version: 0.1"
 startPicture = "Bilder//test.png"
 background = "Bilder//empty.png"
 #https://www.geeksforgeeks.org/python/python-set-background-template-in-kivy/
+
+
+
 
 class myGridLayout(GridLayout):
     version = StringProperty(version)
@@ -31,6 +40,7 @@ class startApp(App):
 
 class levelAuswahlApp(App):
     def build(self):
+        level = get_level_map("Level 1")
         self.title = 'Level'
         return myScrollView()
 
